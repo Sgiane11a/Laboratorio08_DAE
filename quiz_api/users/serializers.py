185ccb7313ef11b85c2dca425ffc39abff6944f2
yudi-star <yudith.pacco@tecsup.edu.pe>
2025-05-10 23:20:14 -0500

    serializers codificado

---
+++ quiz_api/users/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile, QuizAttempt
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ['id', 'user', 'bio', 'avatar', 'created_at']
+
+class QuizAttemptSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    quiz_title = serializers.CharField(source='quiz.title', read_only=True)
+
+    class Meta:
+        model = QuizAttempt
+        fields = ['id', 'user', 'quiz', 'quiz_title', 'score', 'max_score', 'completed_at']
